49bef71d2ae9e044c9e21b6a4338381eb600846b
Alexis <thadelise@gmail.com>
2025-04-04 19:41:36 -0400

    Implemented main scene and basic teams

--- Agents/Scripts/generic_agent.gd
+++ Agents/Scripts/generic_agent.gd
@@ -1,27 +1,25 @@
 extends CharacterBody2D
 class_name generic_agent
 
+# Allows for easy referencing later
+enum agent_type { HUNTER, RUNNER, SLEEPER }
+@export var type : agent_type
+
 # generic agent attributes as required
 @export var speed: int = 1
 @export var strength: int = 1
 
 # other helpful generic agent attributes
-@export var is_red: bool = true
 @export var time_delay: int = 0
 
-# technically could use Vector2, but this forces integer values
-@export var location_x: int = 0
-@export var location_y: int = 0
-
 # abstract functions are not supported by GDscript
-# override this in oter agents
-func tick():
-	printerr("UNIMPLEMENTED!!!!")
-	pass
 
 # helpful generic functions
 func same_team(other_agent: generic_agent):
 	return self.is_red == other_agent.is_red
+
+func release_agent():
+	pass
 
 # setter and getters
 # technically able to be bypassed but include error checking
--- Home Base/Scripts/home_base.gd
+++ Home Base/Scripts/home_base.gd
@@ -1,1 +1,23 @@
 extends Node2D
+signal flag_captured
+
+@export var is_red := true
+@export var team : Array[generic_agent]
+
+@export var release_timer : Timer
+
+
+func _process(delta: float) -> void:
+	pass
+
+func initalize_teams():
+	pass
+
+func deploy_team():
+	for agent in team:
+		if agent.time_delay >= release_timer.wait_time:
+			agent.release_agent()
+			print("Agent releaseed!")
+
+func on_flag_captured():
+	flag_captured.emit()
